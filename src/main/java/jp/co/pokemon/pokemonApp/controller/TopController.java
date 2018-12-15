f4228248ccd081e38457fffea48b7fcccb3c4fe9
empenguin1186 <emperorpenguint1186@gmail.com>
2018-12-15 23:29:11 +0900

    名前を表示

--- src/main/java/jp/co/pokemon/pokemonApp/controller/TopController.java
+++ src/main/java/jp/co/pokemon/pokemonApp/controller/TopController.java
@@ -1,7 +1,6 @@
 package jp.co.pokemon.pokemonApp.controller;
 
 import jp.co.pokemon.pokemonApp.service.TopService;
-import me.sargunvohra.lib.pokekotlin.model.NamedApiResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
